11ef2b65f3a9596a592ca1b4ea77bd124ec8a469
fikris6889-tech <fikris6889@gmail.com>
2026-09-07 16:40:16 +0530

    Create aws_ase_backupCleanup.sh

---
+++ scripts/aws_ase_backupCleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: Fikris Technologies
+#Purpose: Root-level entry point invoked by cron/scheduler; switches to
+#         the sybase SID user and runs the backup-cleanup wrapper.
+DATE="$(date '+%Y%m%d-%H%M%S1')"
+sid=`df -h | grep '/sybase/' | awk '{print $NF}' | cut -d / -f3 | head -1`
+SID=`echo ${sid} | tr "[A-Z]" "[a-z]"`
+
+
+sudo su - syb$SID -c " /usr/sap/ASE_SCRIPTS/wrapper/wrapper_backupCleanup.sh "
